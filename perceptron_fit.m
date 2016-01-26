e6c90232a99428e911b3cb8cf515f809fd0ef84e
Ryan Louie <ryan.louie@students.olin.edu>
2016-01-26 16:50:28 +0800

    Create perceptron_fit.m

---
+++ perceptron_fit.m
@@ -0,0 +1,30 @@
+function W = perceptron_fit(M, lr, n_steps, zero_bias)
+
+    shape = size(M);
+    
+    % initialize weights, bias included
+    W = rand(n_steps + 1,shape(2)-1);
+    if zero_bias
+        % initialize bias to be zero
+        W(:, 1) = zeros(n_steps + 1, 1);
+    end
+
+    
+    X = M(:,1:shape(2)-1);
+    ytrue = M(:,shape(2));
+    ypred = zeros(shape(1),1);
+    error = zeros(shape(1),1);
+
+    % learning procedure
+    for step = 1:n_steps
+        row = mod(step, shape(1)) + 1;
+
+        % perceptron prediction
+        ypred(row) = X(row, :)*W(step,:)' > 0; 
+        error(row) = ytrue(row) - ypred(row);
+
+        % weight update
+        W(step+1, :) = W(step,:) + lr*error(row)*X(row,:);
+    end
+
+end
