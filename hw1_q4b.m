612da61d583d3dfc33253499f0222a40e2bf5bf8
themythicaldrago <ryan.louie@students.olin.edu>
2016-01-28 08:49:36 -0500

    fixed plotting of LMS slope line

--- hw1_q4b.m
+++ hw1_q4b.m
@@ -10,8 +10,12 @@ M = [[1 0.9 8.8],
      [1 3.0 5.5],
      [1 3.6 2.2],
      [1 4.0 0.7]];
+shape = size(M);
 
-[W, error] = LMS_fit(M, 2, 100, false);
+learning_rate = 0.02;
+epochs = 100;
+n_steps = epochs * shape(1);
+[W, error] = LMS_fit(M, learning_rate, n_steps, true);
 
 % plot the weight trajectories
 subplot(1,2,1);
@@ -25,8 +29,10 @@ subplot(1,2,2);
 plot(M(:,2), M(:,3), 'o')
 % visualize least squares fit
 hold on;
-shape = size(W);
+
 x_range = 0:5;
-y_range = W(shape(2), 2)*x_range + W(shape(2), 1);
+slope = W(n_steps + 1, 2)
+intercept = W(n_steps + 1, 1)
+y_range = slope*x_range + intercept;
 plot(x_range,y_range, '--');
 title('LMS fitting result')
