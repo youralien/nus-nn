2ca3f077949f6ea4c9ba756e7d1ab87e80029fa8
Ryan Christopher Louie <e0014547@nus.edu.sg>
2016-02-29 18:05:56 +0800

    converted MATLAB data into open csv format

---
+++ nus-nnet-hw2-faces/dataconvert.m
@@ -0,0 +1,33 @@
+split = 'Train';
+directory = [split,'Images'];
+[status, list] = system( ['dir ', directory, '\*.att ', '/B /S' ] );
+result = textscan( list, '%s', 'delimiter', '\n' );
+labelList = result{1};
+
+[status, list] = system( ['dir ', directory, '\*.jpg ', '/B /S' ] );
+result = textscan( list, '%s', 'delimiter', '\n' );
+imageList = result{1};
+
+side_length = 101;
+shape = size(labelList);
+n_examples = shape(1);
+n_features = side_length^2;
+Y = zeros(n_examples);
+X = zeros(n_examples, n_features);
+
+for count=1:n_examples
+    labelFn = char(labelList(count));
+    imageFn = char(imageList(count));
+    
+    L = load(labelFn);
+    % get gender, first element
+    Y(count) = L(1);
+    
+    im = rgb2gray(imread(imageFn));
+    im = imresize(im, [side_length, side_length]);
+    X(count,:) = im(:); % flatten
+    
+end
+
+csvwrite([split, 'Feats.csv'], X)
+csvwrite([split, 'Labels.csv'], Y)
