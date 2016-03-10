fe861897136427dbb68e120574edece3261d2da8
youralien <ryan.louie@students.olin.edu>
2016-03-10 18:40:25 +0800

    Fixed neighborhood function TYPOS, no exploding weights

--- mnist/3_som.py
+++ mnist/3_som.py
@@ -1,12 +1,7 @@
 import numpy as np
 import sys
 from load import mnist
-# from foxhound.utils.vis import grayscale_grid_vis, unit_scale
-from scipy.misc import imsave
 from scipy.spatial.distance import cdist
-
-def init_weights(shape):
-    return np.random.randn(*shape) * 0.01
 
 def idx1D_to_idx2D(idx, shape):
     n_rows, n_cols = shape
@@ -31,7 +26,7 @@ def time_varying_neighborhood_function(d, n, sigma_0, tau):
     d: distance from neighbor
     n: iteration. n=0 is the start of time
     """
-    h = np.exp( d**2 / 2.*time_varying_sigma(n, sigma_0, tau) )
+    h = np.exp( d**2 / (2.*time_varying_sigma(n, sigma_0, tau)**2) )
     return h
 
 def learningRate(n, lr_0, n_epochs_organizing_phase, lr_min=0.01):
@@ -50,7 +45,9 @@ def init_neighborhood_size(map_shape):
 def init_timeconstant(n_epochs_organizing_phase, sigma_0):
     return float(n_epochs_organizing_phase) / np.log(sigma_0)
 
-trX, teX, trY, teY = mnist(onehot=True)
+trX, teX, trY, teY = mnist(ntrain=2000, ntest=1000, onehot=True)
+xmin_val = trX[0].min()
+xmax_val = trX[0].max()
 
 if raw_input('remove_classes 3 and 4? (y/n)') == 'y':
     trY = np.column_stack((trY[:,:3], trY[:,5:]))
@@ -62,12 +59,9 @@ else:
     assert teY.shape[1] == 10
     n_classes = 10
 
-# X = np.random.randn(1,784)
-# Y = np.random.randn(1,8)
-
 map_shape = (5, 4)
 map_size = map_shape[0] * map_shape[1]
-w = init_weights((784, map_size))
+w = np.random.uniform(xmin_val, xmax_val, (784, map_size))
 
 n_epochs_organizing_phase = 1000;
 sigma_0 = init_neighborhood_size(map_shape)
@@ -110,22 +104,7 @@ for n in range(n_epochs_organizing_phase):
             print "winner: ", winner_i, winner_j
             print "neighbors: \n", neighbors
             print "map_distances: \n", map_distances.reshape(map_shape)
+        
 
         sys.stdout.flush()
     raw_input("continue epoch %d?" % (n+1))
-
-# py_x = model(X, w)
-# y_x = T.argmax(py_x, axis=1)
-
-# cost = T.mean(T.nnet.categorical_crossentropy(py_x, Y))
-# params = [w, w_o]
-# updates = sgd(cost, params)
-
-# train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
-# predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
-
-# for i in range(100):
-#     for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)):
-#         cost = train(trX[start:end], trY[start:end])
-#     print np.mean(np.argmax(teY, axis=1) == predict(teX))
-
