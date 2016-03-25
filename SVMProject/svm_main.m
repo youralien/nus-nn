ebd8a229f93af5d64837120d32b476ef669c9c0c
themythicaldrago <ryan.louie@students.olin.edu>
2016-03-25 13:13:24 +0800

    Visualization for 2D for non-linear decision boundaries, and attempts at prediction using only support vectors

--- SVMProject/svm_main.m
+++ SVMProject/svm_main.m
@@ -1,4 +1,5 @@
-load('train_tls.mat') % TODO remove this when submitting
+dataset = '_cls';
+load(strcat(['train' dataset '.mat'])) % TODO remove this when submitting
 trX = data;
 trY = label;
 
@@ -8,13 +9,13 @@ mu = mean(trX, 2);
 stdev = std(trX, 0, 2);
 trX = bsxfun(@rdivide, bsxfun(@minus, trX, mu), stdev);
 
-% SVM fit
+% -- FIT
 margin_type = 'hard';
 kernel_type = 'poly';
 if strcmp(margin_type,'hard')
-    C = 10^6; 
+    C = 10^6;
 else % soft margin
-    C = 1;
+    C = 0.1;
 end
 if strcmp(kernel_type,'linear')
     h1 = trY*trY'; % d_i * d_j
@@ -23,7 +24,7 @@ if strcmp(kernel_type,'linear')
 else % polynomial kernel
     p = 2;
     h1 = trY*trY'; % d_i * d_j
-    K = (trX'*trX + 1)^p; % K(X_i, X_j) = Where K is Polynomial
+    K = (trX'*trX + 1).^p; % K(X_i, X_j) = Where K is Polynomial
     H = bsxfun(@times, h1, K); % d_i * d_j * K(X_i, X_j)
 end
 
@@ -40,18 +41,53 @@ thresh = graythresh(alpha);
 sv_idx = find(alpha > thresh); % support vectors are the non-zeroish vectors
 w = sum(bsxfun(@times, bsxfun(@times, alpha, trY), trX'));
 random_sv = sv_idx(randperm(length(sv_idx),1));
-b = 1 / trY(random_sv) - w*trX(:,random_sv);
-% 2D viz
-hold on
-gscatter(trX(1, :), trX(2, :), trY)
-t = -2:0.1:2;
-x2 = -1*(w(1)*t+b)/w(2);
-plot(t, x2);
-hold off
+b = 1 / trY(random_sv) - w*trX(:,random_sv); % use support vectors only
 
 % load test data
-load('test_ls.mat');
+load(strcat(['test' dataset '.mat']));
 teX = data;
 teY = label;
 % normalize test data
 teX = bsxfun(@rdivide, bsxfun(@minus, teX, mu), stdev);
+
+% -- PREDICT
+% when calculating g, discriminant function, use support vectors only
+Xnew = trX; % what do you want to predict?
+if strcmp(kernel_type,'linear')
+    Kpred = trX(:,sv_idx)'*Xnew;
+else
+    Kpred = (trX(:,sv_idx)'*Xnew + 1).^p;
+end
+g0 = bsxfun(@times, alpha(sv_idx), trY(sv_idx));
+g =  g0'*Kpred;
+predY = sign(g);
+acc = mean(predY == trY')
+
+% 2D viz
+if strcmp(dataset,'') == 0 % if its not the full dataset
+    clf
+    hold on
+    gscatter(trX(1, :), trX(2, :), trY)
+    if strcmp(kernel_type,'linear')
+        t = -2:0.1:2;
+        x2 = -1*(w(1)*t+b)/w(2);
+        plot(t, x2, 'b--');
+    else
+        grid_step = 0.03;
+        x1 = -2:grid_step:2;
+        x2 = -2:grid_step:2;
+        [X1,X2] = ndgrid(x1,x2);
+        Xbound = [X1(:), X2(:)]';
+        if strcmp(kernel_type,'linear')
+            Kpred = trX(:,sv_idx)'*Xbound;
+        else
+            Kpred = (trX(:,sv_idx)'*Xbound + 1).^p;
+        end
+        g0 = bsxfun(@times, alpha(sv_idx), trY(sv_idx));
+        g =  g0'*Kpred;
+        bound_idx = find(abs(g) < 0.05);
+        plot(Xbound(1, bound_idx), Xbound(2,bound_idx), 'b.')
+    end
+    hold off
+end
+
