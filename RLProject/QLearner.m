970eeef6fdaadc548b0e51a86d5878ec9d41c731
themythicaldrago <ryan.louie@students.olin.edu>
2016-04-08 14:39:38 +0800

    decay function for epsilon and alpha, plus visualization

--- RLProject/QLearner.m
+++ RLProject/QLearner.m
@@ -1,4 +1,4 @@
-function QLearner
+function ys = QLearner
 % Parameters
 % ----------
 % epsilon: float (0 - 1)
@@ -24,7 +24,7 @@ task1 = load('task1.mat'); % has the reward variable
 Q = zeros(100,4);
 N = zeros(100,4);
 
-    
+
     function a_k = epsilonGreedy(reward, s_k, epsilon)
         if rand() < epsilon % exploration
             valid_actions = find(reward(s_k,:) > 0);
@@ -38,19 +38,43 @@ N = zeros(100,4);
     function s_k1 = transition(s_k, a_k, n_states, shp)
         if nargin < 3 % default to 100 states, 10x10 grid
             n_states = 100;
-            shp = [10 10]; 
+            shp = [10 10];
         end
         action_movements = [
             -1, 0; % up
-             0, 1; % right
-             1, 0; % down
-             0,-1;]; % left
+            0, 1; % right
+            1, 0; % down
+            0,-1;]; % left
         s_k_sub = ind2sub(n_states,s_k); % current state i,j
         s_k1_sub = s_k_sub + action_movements(a_k,:); % next state i,j
         s_k1 = sub2ind(shp, s_k1_sub(1), s_k1_sub(2)); % next state
     end
 
+    function mult = decay(k, type)
+        if type == 1
+            mult = 1 / k;
+        elseif type == 2
+            mult = 100 / (100 + k);
+        elseif type == 3
+            mult = (1 + log(k)) / k;
+        else
+            mult = (1 + 5*log(k)) / k;
+        end
+        if mult > 1
+            mult = 1;
+        end
+    end
+
 s_k = 1
 a_k = epsilonGreedy(task1.reward, s_k, 0.2)
 s_k1 = transition(s_k, a_k)
+
+iter = 300;
+ys = zeros(iter,4);
+for type=1:4
+    for k=1:iter
+        ys(k,type) = decay(k,type);
+    end
+end
+
 end
---
+++ RLProject/RL_play.m
@@ -0,0 +1,3 @@
+ys = QLearner;
+plot(ys);
+legend('1/k', '100/(100+k)', '(1+log(k))/k', '(1+5log(k))/k');
