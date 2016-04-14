ddfea01cd19954b03ba0b3e679e0a05f0141bb73
themythicaldrago <ryan.louie@students.olin.edu>
2016-04-14 14:33:31 +0800

    fixed errors in transition method, Q values are updating for a trial

--- RLProject/QLearner.m
+++ RLProject/QLearner.m
@@ -21,9 +21,6 @@ function ys = QLearner
 % Q: all the updates Q values of the state-action board
 % N: all the updated N values of the state-action board
 task1 = load('task1.mat'); % has the reward variable
-Q = zeros(100,4);
-N = zeros(100,4);
-
 
     function a_k = epsilonGreedy(reward, s_k, epsilon)
         if rand() < epsilon % exploration
@@ -45,8 +42,8 @@ N = zeros(100,4);
             0, 1; % right
             1, 0; % down
             0,-1;]; % left
-        s_k_sub = ind2sub(n_states,s_k); % current state i,j
-        s_k1_sub = s_k_sub + action_movements(a_k,:); % next state i,j
+        [s_k_i, s_k_j] = ind2sub(shp,s_k); % current state i,j
+        s_k1_sub = [s_k_i, s_k_j] + action_movements(a_k,:); % next state i,j
         s_k1 = sub2ind(shp, s_k1_sub(1), s_k1_sub(2)); % next state
     end
 
@@ -65,7 +62,7 @@ N = zeros(100,4);
         end
     end
 
-s_k = 1
+s_k = 11
 a_k = epsilonGreedy(task1.reward, s_k, 0.2)
 s_k1 = transition(s_k, a_k)
 
@@ -75,6 +72,22 @@ for type=1:4
     for k=1:iter
         ys(k,type) = decay(k,type);
     end
+end
+
+% one trial
+discount = 0.9;
+s = ones(3000,1);
+a = ones(3000,1);
+Q = zeros(100,4);
+N = zeros(100,4);
+for k=1:3000
+   alpha = decay(k, 2);
+   epsilon = alpha; % aliasing?
+   a(k) = epsilonGreedy(task1.reward, s(k), epsilon);
+   disp([s(k) a(k)]);
+   s(k+1) = transition(s(k), a(k));
+   N(s(k),a(k)) = N(s(k),a(k)) + 1;
+   Q(s(k),a(k)) = Q(s(k),a(k)) + alpha*(task1.reward(s(k),a(k)) + discount*max(Q(s(k+1),:)) - Q(s(k),a(k)));
 end
 
 end
