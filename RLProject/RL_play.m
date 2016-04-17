334290928e4efef0b7f41c79d9fc1f5f81dcd592
themythicaldrago <ryan.louie@students.olin.edu>
2016-04-17 23:56:24 +0800

    Plotting heatmaps when epsilonGreedy exploits the actions associated with Q, not reward

--- RLProject/RL_play.m
+++ RLProject/RL_play.m
@@ -1,6 +1,9 @@
 load('task1.mat')
-thresh = 0.05;
-decay_type = 1; % this one never worked before...
-[Q, ~, time, success_rate] = QLearnManyTrial(reward, 0.9, decay_type, thresh, 0);
+thresh = 0.5;
+a_decay_type = 2; % this one never worked before...
+e_decay_type = 2;
+gamma = 0.9;
+[Q, N, time, success_rate] = QLearnManyTrial(reward, gamma, a_decay_type, e_decay_type, thresh, 1);
 optimal_policy = calculateOptimalPolicy(Q);
 totalReward = walkOptimalPolicy(optimal_policy, reward);
+heatmap(N);
--- RLProject/epsilonGreedy.m
+++ RLProject/epsilonGreedy.m
@@ -1,30 +1,35 @@
-function a_k = epsilonGreedy(reward, s_k, epsilon)
-% Given a reward function for your game board, a current state, and a value
+function a_k = epsilonGreedy(Q, moveValidity, s_k, epsilon)
+% Given a estimated worth function for your game board, a current state, and a value
 % epsilon which determines how likely you are to explore non-exploitative
 % actions, output the action to take at this state, based on the epsilon
 % greedy strategy.
 
-% reward: reward matrix, size (n_state, n_actions)
+% Q: Q matrix, size (n_state, n_action)
+% moveValidty: move validity matrix, size (n_state, n_actions). -1 denotes an
+%   invalid move, all other values are valid.
 % s_k: current state, int
 % epsilon: epsilon value for the policy, float between (0 - 1)
 
 % a_k: action to take at the current state, int
 
-reward_for_actions = reward(s_k,:);
+validity_for_actions = moveValidity(s_k,:);
+worth_for_actions = Q(s_k,:);
+
+valid_actions = find(validity_for_actions >= 0); % FIXME: >=!!
 
 % BIASED: Don't used based on heatmap viz
-% for reward values equal, max returns first instance, and thus is biased towards actions
-% [~, a_k_exploit] = max(reward_for_actions);
+% for worth values equal, max returns first instance, and thus is biased towards actions
+% [~, a_k_exploit] = max(worth_for_actions);
 
 % UNBIASED: If multiple values equal, take random exploitation
-max_reward = max(reward_for_actions);
-a_k_exploit = find(reward_for_actions == max_reward);
+max_worth = max(worth_for_actions(valid_actions));
+a_k_exploit = valid_actions(find(worth_for_actions(valid_actions) == max_worth)); % get one of 4 indexes (actions) which has should be exploited
 if length(a_k_exploit) > 1
     a_k_exploit = randsample(a_k_exploit, 1); % choose a random exploit option if there are multiple
 end
 
 if rand() < epsilon % exploration
-    valid_actions = find(reward_for_actions > 0);
+    
     % explore choices we would not normally exploit
     while true
         a_k = randsample(valid_actions, 1); % choose one from the valid_actions
