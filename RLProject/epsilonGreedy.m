cfd6a54c08778dc15726a6a1035c0a847149f6e4
themythicaldrago <ryan.louie@students.olin.edu>
2016-04-17 18:14:49 +0800

    fixed epsilon greedy so it is not biased towards actions 1 and 2 (up and right

--- RLProject/epsilonGreedy.m
+++ RLProject/epsilonGreedy.m
@@ -11,8 +11,17 @@ function a_k = epsilonGreedy(reward, s_k, epsilon)
 % a_k: action to take at the current state, int
 
 reward_for_actions = reward(s_k,:);
+
+% BIASED: Don't used based on heatmap viz
 % for reward values equal, max returns first instance, and thus is biased towards actions
-[~, a_k_exploit] = max(reward_for_actions);
+% [~, a_k_exploit] = max(reward_for_actions);
+
+% UNBIASED: If multiple values equal, take random exploitation
+max_reward = max(reward_for_actions);
+a_k_exploit = find(reward_for_actions == max_reward);
+if length(a_k_exploit) > 1
+    a_k_exploit = randsample(a_k_exploit, 1); % choose a random exploit option if there are multiple
+end
 
 if rand() < epsilon % exploration
     valid_actions = find(reward_for_actions > 0);
